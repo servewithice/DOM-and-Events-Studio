38c8ae50a2f593c9b426dc2fb1b2ebbe462153ab
Ariel <ariel.l.makowski@gmail.com>
2022-06-09 12:30:42 -0400

    Take Off!

--- scripts.js
+++ scripts.js
@@ -1,2 +1,21 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+function init () {
+    const takeOffButton = document.getElementById("takeoff");
+    const flightStatus = document.getElementById("flightStatus");
+
+    takeOffButton.addEventListener("click", function() {
+        result = window.confirm("Confirm that the shuttle is ready for takeoff.");
+        if (result) {
+            flightStatus.innerHTML = "Shuttle in flight.";
+            document.getElementById("shuttleBackground").style.backgroundColor = "blue";
+        }
+
+
+    });
+
+
+
+}
+
+window.addEventListener("load", init);
